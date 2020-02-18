c975a0ad62f2cec2ca2e2c5a0919578cdcd34a1c
kortina <kortina@gmail.com>
2020-02-17 23:12:05 -0800

    support both relativePath and uniqueFilename Peek Definitions. Add image. Add packaging scripts

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
-import { basename, dirname, join, normalize, relative } from 'path';
+import { basename, dirname, join, normalize, relative, resolve } from 'path';
+import { existsSync } from 'fs';
 import {
   CompletionItemProvider,
   TextDocument,
@@ -47,6 +48,7 @@ class MarkdownFileCompletionItemProvider implements CompletionItemProvider {
     let t = document.getText(line);
     console.log('line:', t);
     let files = (await workspace.findFiles('**/*')).filter(
+      // TODO: paramaterize extensions
       f => f.scheme == 'file' && f.path.match(/\.(md|markdown)/i)
     );
     let items = files.map(f => {
@@ -61,27 +63,48 @@ class MarkdownFileCompletionItemProvider implements CompletionItemProvider {
 // TODO: read this!
 // https://stackoverflow.com/questions/54285472/vscode-how-to-automatically-jump-to-proper-definition
 class MarkdownDefinitionProvider implements vscode.DefinitionProvider {
-  provideDefinition(
+  public async provideDefinition(
     document: vscode.TextDocument,
     position: vscode.Position,
     token: vscode.CancellationToken
-  ): vscode.ProviderResult<vscode.Definition> {
+  ) {
     console.log('provideDefinition');
 
+    const p = new vscode.Position(0, 0);
+
+    // TODO: paramaterize extensions
     const markdownFileRegex = /[\w\.\-\_\/\\]+\.(md|markdown)/i;
     const range = document.getWordRangeAtPosition(position, markdownFileRegex);
     const selectedWord = document.getText(range);
     console.log('selectedWord', selectedWord);
+    let files: Array<Uri> = [];
+    // selectedWord might be either:
+    // a basename for a unique file in the workspace
+    // or, a relative path to a file
+    // Since, selectedWord is just a string of text from a document,
+    // there is no guarantee uniqueFilenamesConfiguration will tell us
+    // it is not a relative path.
+    // However, only check for basenames in the entire project if:
+    if (uniqueFilenamesConfiguration()) {
+      const filename = selectedWord;
+      // there should be exactly 1 file with name = selecteWord
+      files = (await workspace.findFiles('**/*')).filter(f => {
+        return basename(f.path) == filename;
+      });
+    }
+    // If we did not find any files in the workspace,
+    // see if a file exists at the relative path:
+    if (files.length == 0) {
+      const relativePath = selectedWord;
+      let fromDir = dirname(document.uri.path.toString());
+      const absPath = resolve(fromDir, relativePath);
+      if (existsSync(absPath)) {
+        const f = Uri.file(absPath);
+        files.push(f);
+      }
+    }
 
-    // TODO: find the file named selected-word.md
-    const workspace = vscode.workspace.getWorkspaceFolder(document.uri);
-    const root = workspace ? workspace.uri : document.uri;
-    const f = root.with({
-      path: join(root.path, 'test.md'),
-    });
-    const p = new vscode.Position(0, 0);
-    const l = new vscode.Location(f, p);
-    return [l];
+    return files.map(f => new vscode.Location(f, p));
   }
 }
 
