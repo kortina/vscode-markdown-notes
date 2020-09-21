9bdf3313e332c0e198b4bfb579c26cd4d9def48f
Kortina <kortina@gmail.com>
2020-09-20 22:56:38 -0700

    cleanup

--- src/findNonIgnoredFiles.ts
+++ src/findNonIgnoredFiles.ts
@@ -7,11 +7,6 @@ import { exec, ExecException } from 'child_process';
 // import applicationInsights from './telemetry';
 import { join } from 'path';
 
-// TODO: https://github.com/TomasHubelbauer/vscode-extension-findFilesWithExcludes
-// TODO: https://github.com/Microsoft/vscode/issues/48674 for finding MarkDown files that VS Code considers not ignored
-// TODO: https://github.com/Microsoft/vscode/issues/47645 for finding MarkDown files no matter the extension (VS Code language to extension)
-// TODO: https://github.com/Microsoft/vscode/issues/11838 for maybe telling if file is MarkDown using an API
-// TODO: https://github.com/Microsoft/vscode/blob/release/1.27/extensions/git/src/api/git.d.ts instead of Git shell if possible
 export default async function findNonIgnoredFiles(
   pattern: string,
   checkGitIgnore = true
@@ -28,6 +23,7 @@ export default async function findNonIgnoredFiles(
   return filterGitIgnored(uris);
 }
 
+// TODO: https://github.com/Microsoft/vscode/blob/release/1.27/extensions/git/src/api/git.d.ts instead of git shell if possible
 async function filterGitIgnored(uris: Uri[]): Promise<Uri[]> {
   const workspaceRelativePaths = uris.map((uri) => workspace.asRelativePath(uri, false));
   for (const workspaceDirectory of workspace.workspaceFolders!) {
