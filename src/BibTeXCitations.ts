6631b74fbb5082d5d49ff4b6d418df3358d55f77
Kortina <kortina@gmail.com>
2021-03-02 14:57:18 -0800

    run prettier

--- src/BibTeXCitations.ts
+++ src/BibTeXCitations.ts
@@ -1,5 +1,5 @@
-import * as vscode from "vscode";
-import { readFile, existsSync } from "fs";
+import * as vscode from 'vscode';
+import { readFile, existsSync } from 'fs';
 
 type Config = {
   bibTeXFilePath: string;
@@ -10,9 +10,9 @@ export class BibTeXCitations {
   static _rxBibTeXInLibrary = /^@\S+{(\S+),/gm;
 
   static cfg(): Config {
-    let c = vscode.workspace.getConfiguration("vscodeMarkdownNotes");
+    let c = vscode.workspace.getConfiguration('vscodeMarkdownNotes');
     return {
-      bibTeXFilePath: c.get("bibtexFilePath") as string,
+      bibTeXFilePath: c.get('bibtexFilePath') as string,
     };
   }
 
@@ -32,7 +32,7 @@ export class BibTeXCitations {
     return this.bibTeXFile().then((x) => {
       const pos = this.position(x, citation);
       if (pos == null) {
-        return Promise.reject("Cannot get location");
+        return Promise.reject('Cannot get location');
       } else {
         const uri = vscode.Uri.file(this.bibTexFilePath());
         return new vscode.Location(uri, pos);
@@ -44,22 +44,22 @@ export class BibTeXCitations {
     const path = this.cfg().bibTeXFilePath;
 
     // Absolute path (Unix and Windows)
-    if (path.startsWith("/") || path.indexOf(":\\") == 1) {
+    if (path.startsWith('/') || path.indexOf(':\\') == 1) {
       return path;
     }
 
     // Workspace relative path
     const folders = vscode.workspace?.workspaceFolders;
     if (folders && folders.length > 0) {
-      return folders[0].uri.fsPath.toString() + "/" + path;
+      return folders[0].uri.fsPath.toString() + '/' + path;
     }
     return path;
   }
 
   private static bibTeXFile(): Promise<string> {
     const path = this.bibTexFilePath();
-    if (path == null || path == "") {
-      return Promise.reject("BibTeX file location not set");
+    if (path == null || path == '') {
+      return Promise.reject('BibTeX file location not set');
     }
 
     return new Promise((resolve, reject) => {
@@ -78,15 +78,12 @@ export class BibTeXCitations {
     return Array.from(matches).map((x) => x[1]);
   }
 
-  private static position(
-    data: string,
-    citation: string
-  ): vscode.Position | null {
+  private static position(data: string, citation: string): vscode.Position | null {
     let pos = data.match(citation)?.index;
     if (pos == null) {
       return null;
     }
-    const numLines = data.substring(0, pos).split("\n").length;
+    const numLines = data.substring(0, pos).split('\n').length;
     return new vscode.Position(numLines - 1, 0);
   }
 }
